93ae52073a5aa9815d529cfa669dcf02bc5ab506
wascoyur <mytem20007@gmail.com>
2021-01-08 18:59:36 +0500

    feat server работает

---
+++ back/server.js
@@ -0,0 +1,17 @@
+ const express = require('express');
+ const products = require('../back/data/backend_products');
+
+ const app =express();
+
+app.listen(5000, console.log('server up'))
+
+ app.get('/', (req, res) =>{
+   res.send('API запущен')
+ })
+ app.get('/api/products', (req, res) =>{
+   res.json(products)
+ })
+ app.get('/api/products/:id', (req, res) =>{
+   const product = products.find((p) => p._id === req.params.id)
+   res.json(product)
+ })
