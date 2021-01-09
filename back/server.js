e3e56f35fedb9cf115cad5f070fec253f71e3b69
wascoyur <mytem20007@gmail.com>
2021-01-09 19:40:50 +0500

    prepare: node переключен на es6 модули

--- back/server.js
+++ back/server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const products = require('../back/data/backend_products');
-const dotenv = require('dotenv').config({path:'../back/.env'});
+import express from'express';
+import products from '../back/data/backend_products.js';
+import dotenv from'dotenv';
 
-
+dotenv.config({path:'../back/.env'});
 const app = express();
 
 const PORT = process.env.PORT || 5000 ;
 
 app.listen(
   PORT,
-  console.log(`server up in ${process.env.NODE_ENV} mode port: ${PORT}`)
+  console.log(`server up in mode: ${process.env.NODE_ENV}  port: ${PORT}`)
 );
 
 app.get('/', (req, res) => {
