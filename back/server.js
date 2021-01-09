88cae7ac21933b0a693840259856de25805b3fde
wascoyur <mytem20007@gmail.com>
2021-01-09 17:00:04 +0500

    настроен запуск nodemon из дир front

--- back/server.js
+++ back/server.js
@@ -1,18 +1,25 @@
- const express = require('express');
- const products = require('../back/data/backend_products');
- const dotenv = require('dotenv');
+const express = require('express');
+const products = require('../back/data/backend_products');
+const dotenv = require('dotenv');
 
- const app =express();
+dotenv.config();
 
-app.listen(5000, console.log('server up'))
+const app = express();
 
- app.get('/', (req, res) =>{
-   res.send('API запущен')
- })
- app.get('/api/products', (req, res) =>{
-   res.json(products)
- })
- app.get('/products/api/products/:id', (req, res) =>{
-   const product = products.find((p) => p._id === req.params.id)
-   res.json(product)
- })
+const PORT = process.env.PORT || 5000 ;
+
+app.listen(
+  PORT,
+  console.log(`server up in ${process.env.NODE_ENV} mode port: ${PORT}`)
+);
+
+app.get('/', (req, res) => {
+  res.send('API запущен');
+});
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+app.get('/products/api/products/:id', (req, res) => {
+  const product = products.find(p => p._id === req.params.id);
+  res.json(product);
+});
