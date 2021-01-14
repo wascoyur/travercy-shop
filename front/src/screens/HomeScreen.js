1d75c0f239ed8ddbe972a5251574a23f3c5a17d0
wascoyur <mytem20007@gmail.com>
2021-01-14 12:40:28 +0500

    HomeScreen done

--- front/src/screens/HomeScreen.js
+++ front/src/screens/HomeScreen.js
@@ -1,34 +1,31 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Col, Row } from 'react-bootstrap';
 import Product from '../component/Product';
-import axios from 'axios'
+import { listProducts } from '../actions/productActions';
 
-export const HomeScreen=()=> {
+export const HomeScreen = () => {
+  const dispatch = useDispatch();
+  const productList = useSelector(state => state.productList);
+  const { loading, error, products } = productList;
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+  return (
+    <div>
+      <h3>Последние товары</h3>
+      {loading ? <h2>Загрузка...</h2> : error ? {error} : (<Row>
+        {products.map((item, index) => {
+          return (
+            <Col key={item._id} sm={12} md={6} lg={2}>
+              <Product product={item} />
+            </Col>
+          );
+        })}
+      </Row>)}
 
-    const [products, setProducts] = useState([]);
-    
-    useEffect(()=>{
-      const fetchProducts = async ()=>{
-        const {data} = await axios.get('api/products')
-        setProducts(data)
-      }
-      fetchProducts();
-    },[])
+    </div>
+  );
+};
 
-    return (
-      <div>
-        <h3>Последние товары</h3>
-        <Row>
-          {products.map((item, index) =>{
-            return (
-              <Col key={item._id} sm={12} md={6} lg={2}>
-                  <Product product={item}/>
-              </Col>
-            );
-          })}
-        </Row>
-      </div>
-    );
-}
-
-export default HomeScreen
+export default HomeScreen;
