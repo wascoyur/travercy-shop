52525201ddcfa1003399a76ca1525d4fbcd7a51c
wascoyur <mytem20007@gmail.com>
2021-01-15 11:41:57 +0500

    CartReducer & Add toCart done

--- front/src/screens/CartScreen.js
+++ front/src/screens/CartScreen.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Message from '../component/Message';
+import {} from '../actions/cartActions'
 
-const CartScreen = () => {
+const CartScreen = ({ match, location, history }) => {
+  const productId = match.params.id;
+  
   return (
     <div>
       <h2>CartScreen</h2>
     </div>
-  )
-}
+  );
+};
 
-export default CartScreen
+export default CartScreen;
--- front/src/store.js
+++ front/src/store.js
@@ -1,14 +1,26 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import {  productListReducer, productDetailsReducer } from './reducers/productReducer'
+import {
+  productListReducer,
+  productDetailsReducer,
+} from './reducers/productReducer';
+import { cartReducer } from './reducers/cartReducer';
 
 const reducer = combineReducers({
   productList: productListReducer,
-  productDetails:productDetailsReducer
+  productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
-const initialState = {};
+const cartItemsFromStorage = localStorage.getItem('cardItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : [];
+const initialState = {cart: {cardItems:cartItemsFromStorage}};
 const middleware = [thunk];
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
-export default store
+export default store;
