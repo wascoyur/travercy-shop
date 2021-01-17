50cb1ff09e79e04184270e58d6369088af0d4beb
wascoyur <mytem20007@gmail.com>
2021-01-17 19:42:25 +0500

    featL user auth implement

---
+++ back/controllers/userController.js
@@ -0,0 +1,24 @@
+import User from '../models/userModel.js';
+import asyncHandler from 'express-async-handler';
+
+//@desc Auth user & get token
+//@route GET /api/login
+//@access Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body
+  const user = await User.findOne({ email })
+
+  if (user&& (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: null
+    })
+  } else {
+    res.status(401)
+    throw new Error('Неверный логин или пароль')
+  }
+});
+export{authUser}
