bb4063fc6a1f78dcbddf37ed9f1c8770241237e6
wascoyur <mytem20007@gmail.com>
2021-01-20 10:55:32 +0500

    fix: add protect route ok

--- back/errorMiddleware/authMiddleware.js
+++ back/errorMiddleware/authMiddleware.js
@@ -3,6 +3,8 @@ import User from '../models//userModel.js';
 import asyncHandler from 'express-async-handler';
 
 const protect = asyncHandler(async (req, res, next) => {
-  
+  let token
+  console.log('req.he', req.headers.authorization);
+  next()
 });
 export { protect };
--- back/routes/userRout.js
+++ back/routes/userRout.js
@@ -4,9 +4,10 @@ import {
   authUser,
   getUserProfile
 } from '../controllers/userController.js';
+import {protect} from '../errorMiddleware/authMiddleware.js'
 
 router.post('/login', authUser);
-router.route('/profile').get(getUserProfile);
+router.route('/profile').get(protect, getUserProfile);
 
 
 export default router;
