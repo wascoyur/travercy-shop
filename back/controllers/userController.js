76757f6915453bf6b21c3bfce81571217249c92c
wascoyur <mytem20007@gmail.com>
2021-01-20 10:25:51 +0500

    feat: add Controller method & route

--- back/controllers/userController.js
+++ back/controllers/userController.js
@@ -1,25 +1,33 @@
 import User from '../models/userModel.js';
 import asyncHandler from 'express-async-handler';
-import generateToken from '../utils/generateToken.js'
+import generateToken from '../utils/generateToken.js';
 
 //@desc Auth user & get token
 //@route GET /api/login
 //@access Public
 const authUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body
-  const user = await User.findOne({ email })
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
 
-  if (user&& (await user.matchPassword(password))) {
+  if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: generateToken(user._id)
-    })
+      token: generateToken(user._id),
+    });
   } else {
-    res.status(401)
-    throw new Error('Неверный логин или пароль')
+    res.status(401);
+    throw new Error('Неверный логин или пароль');
   }
 });
-export{authUser}
+
+//@desc Get User profile
+//@route GET /api/profie
+//@access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.send('Success')
+});
+
+export { authUser, getUserProfile };
