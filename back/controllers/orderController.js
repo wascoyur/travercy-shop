d6453c5db53192e34bb5cb7abe9280d6d50dec2a
wascoyur <mytem20007@gmail.com>
2021-02-12 08:59:25 +0500

    feat: 10.1. Get order by ID Endpoint is done

--- back/controllers/orderController.js
+++ back/controllers/orderController.js
@@ -43,80 +43,13 @@ const getOrderById = asyncHandler(async (req, res) => {
     'user',
     'name email'
   );
-
   if (order) {
-    res.json(order);
-  } else {
-    res.status(404);
-    throw new Error('Чек не найден');
-  }
+  res.json(order);
+} else {
+  res.status(404);
+  throw new Error('Чек не найден');
+}
 });
 
-// @desc    Update order to paid
-// @route   GET /api/orders/:id/pay
-// @access  Private
-const updateOrderToPaid = asyncHandler(async (req, res) => {
-  const order = await Order.findById(req.params.id);
 
-  if (order) {
-    order.isPaid = true;
-    order.paidAt = Date.now();
-    order.paymentResult = {
-      id: req.body.id,
-      status: req.body.status,
-      update_time: req.body.update_time,
-      email_address: req.body.payer.email_address,
-    };
-
-    const updatedOrder = await order.save();
-
-    res.json(updatedOrder);
-  } else {
-    res.status(404);
-    throw new Error('Чек не найден');
-  }
-});
-
-// @desc    Update order to delivered
-// @route   GET /api/orders/:id/deliver
-// @access  Private/Admin
-const updateOrderToDelivered = asyncHandler(async (req, res) => {
-  const order = await Order.findById(req.params.id);
-
-  if (order) {
-    order.isDelivered = true;
-    order.deliveredAt = Date.now();
-
-    const updatedOrder = await order.save();
-
-    res.json(updatedOrder);
-  } else {
-    res.status(404);
-    throw new Error('Чек не найден');
-  }
-});
-
-// @desc    Get logged in user orders
-// @route   GET /api/orders/myorders
-// @access  Private
-const getMyOrders = asyncHandler(async (req, res) => {
-  const orders = await Order.find({ user: req.user._id });
-  res.json(orders);
-});
-
-// @desc    Get all orders
-// @route   GET /api/orders
-// @access  Private/Admin
-const getOrders = asyncHandler(async (req, res) => {
-  const orders = await Order.find({}).populate('user', 'id name');
-  res.json(orders);
-});
-
-export {
-  addOrderItems,
-  getOrderById,
-  updateOrderToPaid,
-  updateOrderToDelivered,
-  getMyOrders,
-  getOrders,
-};
+export { addOrderItems, getOrderById };
