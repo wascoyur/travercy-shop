90e2016eb016e8868ee0f4de09f236581843106e
wascoyur <mytem20007@gmail.com>
2021-02-25 11:14:21 +0500

    feat: add backend order list

--- back/routes/orderRouts.js
+++ back/routes/orderRouts.js
@@ -1,10 +1,10 @@
 import express from 'express';
 const router = express.Router();
-import { addOrderItems, getMyOrders, getOrderById, updateOrderToPaid } from '../controllers/orderController.js';
-import { protect } from '../errorMiddleware/authMiddleware.js';
+import { addOrderItems, getMyOrders, getOrderById, getOrders, updateOrderToPaid } from '../controllers/orderController.js';
+import { admin, protect } from '../errorMiddleware/authMiddleware.js';
 
 router.route('/myorders').get(protect, getMyOrders);
-router.route('/').post(protect, addOrderItems);
+router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders);
 router.route('/:id').get(protect, getOrderById);
 router.route('/:id/pay').put(protect, updateOrderToPaid);
 
