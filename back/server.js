3da1f3a20206278555d0f85c6348e6c8c981171e
wascoyur <mytem20007@gmail.com>
2021-03-18 16:55:26 +0500

    14.1.Prepare for deployment is done

--- back/server.js
+++ back/server.js
@@ -17,7 +17,7 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
-const PORT = process.env.PORT || 5000;
+
 
 app.use(express.json());
 app.use('/api/products', productRoutes);
@@ -30,17 +30,25 @@ app.use('/api/config/paypal', (req, res) => {
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/build')))
+  console.log('__dirname,', __dirname);
+
+  app.get('*', (req,res) =>{res.sendFile(path.resolve(__dirname, 'front','build','index.html'))})
+} else {
+  app.get('/', (req, res) => {
+  res.send('API запущен');
+});
+}
+
 app.use(notFound);
 
 app.use(errorHandler);
 
+const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
   console.log(
     `server up in mode: ${process.env.NODE_ENV}  port: ${PORT}`.yellow.bold
   )
 );
-
-app.get('/', (req, res) => {
-  res.send('API запущен');
-});
